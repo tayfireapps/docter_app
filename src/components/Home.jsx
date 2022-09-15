0d40a1300148a2a111607d93c420f77bb9a8aedf
tayfireapps <taynt@fireapps.vn>
2022-09-15 17:28:58 +0700

    update code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,16 @@
 
 import React from "react";
-import { Button, Menu, Container, Grid, Paper } from '@mui/material';
-import { SaveOutlined } from '@mui/icons-material';
-
-import { makeStyles } from "@mui/styles";
-import { red } from "@mui/material/colors";
+import { 
+    Button,
+    Container, 
+    Grid, 
+    Card, 
+    CardContent,
+    CardActions,
+    Typography
+} from '@mui/material';
 
 const Home = function(props) {
-    const useStyle = makeStyles({
-        root: {
-            border: 0
-        }
-    });
-    const classes = useStyle();
     return (
         <>
         <Container maxWidth="lg">
@@ -22,12 +20,56 @@ const Home = function(props) {
                     <Grid item lg={4}>Logo</Grid>
                     <Grid item lg={8}>Breadcrumbs</Grid>
                 </Grid>
-                <Grid con></Grid>
+                <div className="slider"></div>
             </header>
             <section>
-                <article></article>
-                <article></article>
+                <article className="service">
+                    <Grid container spacing={2}>
+                        <Grid item lg={4}>Service 1</Grid>
+                        <Grid item lg={4}>Service 2</Grid>
+                        <Grid item lg={4}>Service 3</Grid>
+                    </Grid>
+                </article>
+                <article className="why_us">
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Word of the Day
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                        AAAAAA
+                        </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        adjective
+                        </Typography>
+                        <Typography variant="body2">
+                        well meaning and kindly.
+                        <br />
+                        {'"a benevolent smile"'}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                    </Card>
+                </article>
+                <article className="our_docter">
+                    <Grid container spacing={2}>
+                        <Grid item lg={6}>Service 1</Grid>
+                        <Grid item lg={6}>Service 2</Grid>
+                        <Grid item lg={6}>Service 3</Grid>
+                        <Grid item lg={6}>Service 3</Grid>
+                    </Grid>
+                </article>
             </section>
+            <footer>
+                <Grid container spacing={2}>
+                    <Grid item lg={3}>Service 1</Grid>
+                    <Grid item lg={3}>Service 2</Grid>
+                    <Grid item lg={3}>Service 3</Grid>
+                    <Grid item lg={3}>Service 3</Grid>
+                </Grid>
+            </footer>
         {/* <Button startIcon={<SaveOutlined />} variant="contained" color="primary" >Test</Button>
         
         <div className={classes.root}></div>
